4e76448250a3b2825a225b47014799f50ab9cac4
Solntseva Tatyana <825-stv19u648@users.noreply.git.iu7.bmstu.ru>
2020-04-29 23:41:52 +0300

    changing themes, screen for adding the text

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
-from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition 
 from kivy.core.window import Window
 from kivymd.app import MDApp
-from kivymd.uix.backdrop import *
-from kivy.metrics import dp 
+from kivymd.theming import ThemeManager
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivymd.uix.list import OneLineListItem, IRightBodyTouch
+from kivymd.uix.selectioncontrol import MDSwitch
 
 Window.size = (360, 720)
 
+
 class Colours:
-    Screen.b_ground_color = (1.0, 0.62, 0.0, 0.5)
-    name_of_app_color = (1.0, 0.62, 0.0, 0.3)
+    bg_color = (1.0, 0.62, 0.0, 0.5)
+    app_name_color = (1.0, 0.62, 0.0, 0.3)
     menu_color = (1.0, 0.62, 0.0, 0.4)
 
-class LearningScreen(Screen, Colours):
-    pass
-
-class ListoftextsScreen(Screen, Colours):
-    pass
-
-class SettingsScreen(Screen, Colours):
-    pass
-
-class AddingTextScreen(Screen, Colours):
-    pass
 
 class TestApp(MDApp):
-    
-    def build(self):
-        self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_hue = "100"
-        sm = ScreenManager(transition = WipeTransition())
-        sm.add_widget(LearningScreen(name='Learning'))
-        sm.add_widget(ListoftextsScreen(name = 'Listoftexts'))
-        sm.add_widget(SettingsScreen(name='Settings'))
-        sm.add_widget(AddingTextScreen(name='AddText'))
-        return sm
+    def change_theme(self, checkbox, value):
+        if value:
+            self.theme_cls.theme_style = "Dark"
+        else:
+            self.theme_cls.theme_style = "Light"
 
-if __name__ == '__main__':
-    TestApp().run()
+    def __init__(self, **kwargs):
+        self.theme_cls = ThemeManager()
+        self.theme_cls.primary_palette = "DeepOrange"
+        self.theme_cls.theme_style = "Light"
+        super().__init__(**kwargs)
+
+TestApp().run()
